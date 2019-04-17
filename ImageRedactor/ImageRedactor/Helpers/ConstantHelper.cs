f585a2593708530d32cf57a70e7d1acb5628fd42
Alexei Fokin <Zanuda.Lidchanin@gmail.com>
2019-04-17 13:10:06 +0300

    Added images.

---
+++ ImageRedactor/ImageRedactor/Helpers/ConstantHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace ImageRedactor.Helpers
+{
+    public static class ConstantHelper
+    {
+        #region Filenames
+
+        public const string Banana1Image = "banana1.png";
+        public const string Banana3Image = "banana3.png";
+        public const string Mask1Image = "mask1.png";
+        public const string Mask2Image = "mask2.png";
+        public const string Mask3Image = "mask3.png";
+        public const string Monkey1Image = "monkey1.png";
+        public const string Monkey2Image = "monkey2.png";
+        public const string Monkey3Image = "monkey3.png";
+
+        #endregion Filenames
+    }
+}
