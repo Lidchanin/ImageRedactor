100f33de0c5288415832536f58cd57a86e216c05
ahouhel <aliaksandr.Houhel@cogniteq.com>
2019-04-17 12:50:24 +0300

    touch effect example

--- ImageRedactor/ImageRedactor/Pages/SSRealizationPage.xaml.cs
+++ ImageRedactor/ImageRedactor/Pages/SSRealizationPage.xaml.cs
@@ -21,10 +21,29 @@ namespace ImageRedactor.Pages
             _skiaView.PaintSurface += SkiaViewPaintSurfaceHangle;
 
             Content = _skiaView;
+
+            //Effects.Add()
         }
 
         void SkiaViewPaintSurfaceHangle(object sender, SKPaintSurfaceEventArgs e)
         {
+        }
+
+        void Handle_TouchAction(object sender, TouchActionEventArgs args)
+        {
+            switch (args.Type)
+            {
+                case TouchActionType.Pressed:
+                    break;
+                case TouchActionType.Moved:
+                    break;
+                case TouchActionType.Released:
+                case TouchActionType.Cancelled:
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
